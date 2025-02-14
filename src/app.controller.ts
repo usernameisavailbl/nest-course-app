cd34c9fcc345bb3e181d98b7e21e7087629e20b0
Natdanai Choochan <6730613034@psu.ac.th>
2025-02-14 10:10:41 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/showname')
+  showName(): string {
+    return this.appService.getName();
+  }
+
+  @Get('/showinformation')
+  showInfo(): string {
+    return this.appService.getInfo();
+  }
+
+  @Get('/showjson')
+  myJSON() {
+    return this.appService.getJSON();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'we love nestJS';
+  }
+
+  getName(): string {
+    return 'Natdanai';
+  }
+
+  getInfo(): string {
+    return 'Hello, I am Natdanai Choochan, 18 years old';
+  }
+  
+  getJSON() {
+    return {
+    name: 'Natdanai',
+    lastname: 'Choochan',
+    age: 18};
+  }
+}
